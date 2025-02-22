7e0e7a37f890b7bdb3fc391ee23ea746b6db1de7
harshitthareja <118681259+harshitthareja@users.noreply.github.com>
2025-02-22 23:48:08 +0530

    Initial commit

--- preprocessor.py
+++ preprocessor.py
@@ -2,14 +2,30 @@ import re
 import pandas as pd
 
 def preprocess(data):
-    pattern = r'\d{1,2}/\d{1,2}/\d{2,4},\s\d{1,2}:\d{2}\s-\s'
+    # Define regex patterns for 24-hour and 12-hour (AM/PM) time formats
+    pattern_24hr = r'\d{1,2}/\d{1,2}/\d{2,4},\s\d{1,2}:\d{2}\s-\s'
+    pattern_12hr = r'\d{1,2}/\d{1,2}/\d{2,4},\s*\d{1,2}:\d{2}\s*(?:am|pm)?\s*-\s'
+
+    # Detect time format in the input data
+    if re.search(r'\d{1,2}:\d{2}\s*(?:am|pm)', data, re.IGNORECASE):
+        pattern = pattern_12hr  # Use 12-hour format pattern
+        am_pm_format = True
+    else:
+        pattern = pattern_24hr  # Use 24-hour format pattern
+        am_pm_format = False
 
     messages = re.split(pattern, data)[1:]
-
     dates = re.findall(pattern, data)
 
     df = pd.DataFrame({'user_message': messages, 'message_date': dates})
-    df['message_date'] = pd.to_datetime(df['message_date'], format='%d/%m/%y, %H:%M - ')
+
+    # Convert date column based on detected time format
+    if am_pm_format:
+        df['message_date'] = df['message_date'].str.replace('\u202F', ' ', regex=True)
+        df['message_date'] = pd.to_datetime(df['message_date'], format='%d/%m/%y, %I:%M %p - ')
+    else:
+        df['message_date'] = pd.to_datetime(df['message_date'], format='%d/%m/%y, %H:%M - ')
+
     df.rename(columns={'message_date': 'date'}, inplace=True)
 
     users = []
@@ -26,6 +42,7 @@ def preprocess(data):
     df['message'] = messages
     df.drop(columns=['user_message'], inplace=True)
 
+    # Extract date components
     df['only_date'] = df['date'].dt.date
     df['year'] = df['date'].dt.year
     df['month_num'] = df['date'].dt.month
@@ -35,12 +52,13 @@ def preprocess(data):
     df['hour'] = df['date'].dt.hour
     df['minute'] = df['date'].dt.minute
 
+    # Generate period column
     period = []
     for hour in df[['day_name', 'hour']]['hour']:
         if hour == 23:
-            period.append(str(hour) + "-" + str('00'))
+            period.append(str(hour) + "-00")
         elif hour == 0:
-            period.append(str('00') + "-" + str(hour + 1))
+            period.append("00-" + str(hour + 1))
         else:
             period.append(str(hour) + "-" + str(hour + 1))
 
